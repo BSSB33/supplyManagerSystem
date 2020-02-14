2a7d76faa47d5138e821c8eb41565bc22b863d89
Vitrai Gábor <vitraig@gmail.com>
2020-02-14 16:30:56 +0100

    Empty Project Added via Spring Initializr

---
+++ src/main/java/com/elte/supplymanagersystem/SupplyManagerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.elte.supplymanagersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyManagerSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupplyManagerSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elte/supplymanagersystem/SupplyManagerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elte.supplymanagersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyManagerSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

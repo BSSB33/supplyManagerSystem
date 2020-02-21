df5169804a2ab852f35cc7daa49547dc4aba41f2
Vitrai Gábor <vitraig@gmail.com>
2020-02-21 20:52:20 +0100

    UserController/@GetMapping("") still broken - Under debugging
    Might delete this commit

--- src/main/java/com/elte/supplymanagersystem/controllers/UserController.java
+++ src/main/java/com/elte/supplymanagersystem/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.elte.supplymanagersystem.entities.Role;
 import com.elte.supplymanagersystem.entities.User;
 import com.elte.supplymanagersystem.repositories.UserRepository;
 import com.elte.supplymanagersystem.security.AuthenticatedUser;
+import com.sun.tools.sjavac.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.Option;
 import javax.validation.constraints.Min;
 import java.util.ArrayList;
 import java.util.Optional;
@@ -42,14 +44,17 @@ public class UserController {
 
                 return ResponseEntity.ok(userRepository.findAll());
             }
-            /*else if(loggedInUser.get().getRole() == Role.ROLE_DIRECTOR){
+            else if(loggedInUser.get().getRole() == Role.ROLE_DIRECTOR){
 
                 ArrayList<User> employees = new ArrayList<>();
-                for (User user : userRepository.findAll()){
-                    if(user.getCompany().getId() == loggedInUser.get().getCompany().getId()) employees.add(user);
-                }
-                return ResponseEntity.ok(employees);
-            }*/
+                userRepository.findAll().forEach(user -> {
+                        System.out.println(user.getCompany().getName() + " - " + loggedInUser.get().getCompany().getName() + " " + user.getCompany().getId().equals(loggedInUser.get().getCompany().getId()));
+                    }
+                );
+                //return ResponseEntity.ok(employees);
+                return ResponseEntity.notFound().build();
+
+            }
             else return ResponseEntity.notFound().build();
         }
         else {
